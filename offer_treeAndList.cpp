bd9b00b9bc29d940999eba84e6c820a17f494ecf
ttfutt <1129376438@qq.com>
2019-01-26 23:53:47 +0800

    Create offer_treeAndList.cpp

---
+++ offer_treeAndList.cpp
@@ -0,0 +1,40 @@
+//author : xcj
+//date   : 20190126
+//problem: 包含min函数的栈
+//method : 二叉搜索树的中序遍历就是有序的，根据中序遍历来构建双向链表即可。
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    TreeNode* Convert(TreeNode* pRootOfTree)
+    {
+        TreeNode* pRootOfList = NULL;
+        TreeNode* pTailOfList = NULL;
+        ConvertImp(pRootOfTree, pRootOfList, pTailOfList);
+        return pRootOfList;
+    }
+    
+    void ConvertImp(TreeNode* p, TreeNode* &pRootOfList, TreeNode* &pTailOfList) {
+        if(p == NULL) return;
+        ConvertImp(p->left, pRootOfList, pTailOfList);
+        TreeNode* pr = p->right;
+        if(pRootOfList == NULL) {
+            pRootOfList = p;
+            pTailOfList = p;
+        }
+        else {
+            pTailOfList->right = p;
+            p->left = pTailOfList;
+            p->right = NULL;
+            pTailOfList = p;
+        }
+        ConvertImp(pr, pRootOfList, pTailOfList);
+    }
+};
